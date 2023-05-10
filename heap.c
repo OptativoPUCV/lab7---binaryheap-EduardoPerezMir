474cfef37277ca2f326fe2837feddfbf6908ed5a
Replit user <>
2023-05-10 00:20:39 +0000

    new try

--- heap.c
+++ heap.c
@@ -34,10 +34,10 @@ void heap_pop(Heap* pq){
 
 Heap* createHeap(){
     Heap* newHeap = NULL;
-    newHeap = (Heap*) calloc(sizeof(Heap));
+    newHeap = (Heap*) calloc(1, sizeof(Heap));
     if (newHeap == NULL) return NULL;
     newHeap->capac = 3;
-    newHeap->heapArray = calloc(sizeof(heapElem));
+    newHeap->heapArray = calloc(1, sizeof(heapElem));
     if (newHeap->heapArray== NULL) return NULL;
     
     return newHeap;
